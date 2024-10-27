26c818a3cd127e03302a8ab0d10207d3f2158f42
R3dMage <jonr79@hotmail.com>
2024-10-27 09:55:02 -0400

    Make card into a link

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,36 @@
-import { View } from "react-native";
+import { View, StyleSheet, Pressable } from "react-native";
+import { Link } from "expo-router";
 import {Avatar, Button, Card, Text } from "react-native-paper"
 
-const LeftContent = (props) => <Avatar.Icon {...props} icon="biohazard" />;
+const LeftContent = props => <Avatar.Icon {...props} icon="biohazard" />;
+const GameSelected = () => {
+
+}
 
 export default function index(){
     return (
         <View>
-            <Card>
-                <Card.Title title="John Smith" subtitle="100 - 150" left={LeftContent}/>
-            </Card>
+            <View>
+                <Text style={styles.heading}>Your Move</Text>
+            </View>
+
+            <Link className="card" href="/game" asChild>
+                <Card onPress={GameSelected}>                
+                    <Card.Title title="John Smith" subtitle="100 - 150">
+                    </Card.Title>
+                </Card>
+            </Link>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    heading: {
+        width: 320,
+        height: 30,
+        marginHorizontal: 20,
+        marginVertical: 20,
+        padding: 3,
+        fontSize: 20,
+    },
+});
